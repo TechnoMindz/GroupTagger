1d7cc9545b77d0a18c2e93558532e1a12c9f3dad
Techno Mindz <97782515+TechnoMindz@users.noreply.github.com>
2022-06-24 23:34:31 +0530

    TM Group Tagger

---
+++ Hacker.py
@@ -0,0 +1,123 @@
+import os, logging, asyncio
+from telethon import Button
+from telethon import TelegramClient, events
+from telethon.tl.types import ChannelParticipantAdmin
+from telethon.tl.types import ChannelParticipantCreator
+from telethon.tl.functions.channels import GetParticipantRequest
+from telethon.errors import UserNotParticipantError
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(name)s - [%(levelname)s] - %(message)s'
+)
+LOGGER = logging.getLogger(__name__)
+
+api_id = int(os.environ.get("APP_ID"))
+api_hash = os.environ.get("API_HASH")
+bot_token = os.environ.get("TOKEN")
+client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
+spam_chats = []
+
+@client.on(events.NewMessage(pattern="^/start$"))
+async def start(event):
+  await event.reply(
+    "__**𝐈'𝐦 𝐓𝐚𝐠𝐀𝐥𝐥 𝐁𝐨𝐭**, 𝐢 𝐂𝐚𝐧 𝐌𝐞𝐧𝐭𝐢𝐨𝐧 𝐀𝐥𝐥 𝐌𝐞𝐦𝐛𝐞𝐫𝐬 𝐈𝐧 𝐆𝐫𝐨𝐮𝐩 𝐎𝐫 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 👻\n𝐂𝐥𝐢𝐜𝐤 **/help** 𝐅𝐨𝐫 𝐌𝐨𝐫𝐞 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧__\n\n 𝐅𝐨𝐥𝐥𝐨𝐰 [TECHNO MINDZ](https://t.me/TechnoMindzChat) 𝗢𝗻 𝐓𝐞𝐥𝐞𝐆𝐫𝐚𝐦",
+    link_preview=False,
+    buttons=(
+      [
+        Button.url('✨𝙎𝙐𝙋𝙋𝙊𝙍𝙏', 'https://t.me/TechnoMindzChat'),
+        Button.url('📦 𝙎𝙊𝙐𝙍𝘾𝙀', 'https://github.com/TechnoMindz/GroupTagger')
+      ]
+    )
+  )
+
+@client.on(events.NewMessage(pattern="^/help$"))
+async def help(event):
+  helptext = "**Help Menu of TagAllBot**\n\nCommand: /all\n__You can use this command with text what you want to mention others.__\nExample: `/all Good Morning!`\n__You can you this command as a reply to any message. Bot will tag users to that replied messsage__.\n\nUse /cancel To Stop Tagging in group\n\nFollow [TECHNO MINDZ](https://t.me/technomindzchat) 𝗢𝗡 𝗧𝗘𝗟𝗘𝗚𝗥𝗔𝗠"
+  await event.reply(
+    helptext,
+    link_preview=False,
+    buttons=(
+      [
+        Button.url('✨𝙎𝙐𝙋𝙋𝙊𝙍𝙏', 'https://t.me/technomindzchat'),
+        Button.url('📦 𝙎𝙊𝙐𝙍𝘾𝙀', 'https://github.com/TechnoMindz/GroupTagger')
+      ]
+    )
+  )
+  
+@client.on(events.NewMessage(pattern="^/all ?(.*)"))
+async def all(event):
+  chat_id = event.chat_id
+  if event.is_private:
+    return await event.respond("__This command Can Be Use In Groups And Channels @TechnoMindzChat !__")
+  
+  is_admin = False
+  try:
+    partici_ = await client(GetParticipantRequest(
+      event.chat_id,
+      event.sender_id
+    ))
+  except UserNotParticipantError:
+    is_admin = False
+  else:
+    if (
+      isinstance(
+        partici_.participant,
+        (
+          ChannelParticipantAdmin,
+          ChannelParticipantCreator
+        )
+      )
+    ):
+      is_admin = True
+  if not is_admin:
+    return await event.respond("__Only Admins Can Mention All\n\nFor Help Ask Here @TechnoMindzChat !__")
+  
+  if event.pattern_match.group(1) and event.is_reply:
+    return await event.respond("__Give me one argument!__")
+  elif event.pattern_match.group(1):
+    mode = "text_on_cmd"
+    msg = event.pattern_match.group(1)
+  elif event.is_reply:
+    mode = "text_on_reply"
+    msg = await event.get_reply_message()
+    if msg == None:
+        return await event.respond("__I Can't Mention Members For Older Messages! (messages which are sent before I'm added to group)__")
+  else:
+    return await event.respond("__Reply To a Message Or Give Me Some Text To Mention Others\n\nMade bY @TechnoMindzChat !__")
+  
+  spam_chats.append(chat_id)
+  usrnum = 0
+  usrtxt = ''
+  async for usr in client.iter_participants(chat_id):
+    if not chat_id in spam_chats:
+      break
+    usrnum += 1
+    usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
+    if usrnum == 5:
+      if mode == "text_on_cmd":
+        txt = f"{usrtxt}\n\n{msg}\n\nMade bY @TechnoMindzChat ✌️🔥"
+        await client.send_message(chat_id, txt)
+      elif mode == "text_on_reply":
+        await msg.reply(usrtxt)
+      await asyncio.sleep(2)
+      usrnum = 0
+      usrtxt = ''
+  try:
+    spam_chats.remove(chat_id)
+  except:
+    pass
+
+@client.on(events.NewMessage(pattern="^/cancel$"))
+async def cancel_spam(event):
+  if not event.chat_id in spam_chats:
+    return await event.respond('__There Is No Proccess On Going @TechnoMindzChat...__')
+  else:
+    try:
+      spam_chats.remove(event.chat_id)
+    except:
+      pass
+    return await event.respond('__Stopped.__')
+
+print(">> Techno Mindz Tag All Started @TechnoMindzChat<<")
+client.run_until_disconnected()
---
+++ README.md
@@ -0,0 +1,37 @@
+<h2 align="centre"><i><b>🎀TECHNO MINDZ TAGGER🎀</i></b></h2>
+
+
+
+<p align="center"><a href="https://t.me/technomindzchat"><img src="https://telegra.ph/file/b417bdd01331179d5787c.jpg" width="300"></a></p>
+
+## Introduction
+
+![Typing SVG](https://readme-typing-svg.herokuapp.com/?lines=Welcome+To+Techno+Mindz!;A+Tagger+BOT+For+Groups!;Specially+Created+by+RAVINANDAN!;A+powerful+Tagger+Bot!;Don't+Forget+To+Subcribe;Techno+Mindz+in+YouTube;)
+</p>
+</h1>
+
+## IMP VARS
+
+- API_ID :- GET IT FROM MY.TELEGRAM.ORG 
+- API_HASH :- GET IT FROM MY.TELEGRAM.ORG
+- TOKEN :- GET IT FROM [@BOTFATHER](https://t.me/botfather)
+    
+## DEPLOY
+
+<details><summary>Deploy To Heroku</summary>
+<p>
+<br>
+<a href="https://heroku.com/deploy?template=https://github.com/TechnoMindz/GroupTagger">
+  <img src="https://www.herokucdn.com/deploy/button.svg" alt="Deploy">
+</a>
+</p>
+</details>
+    
+### ꜱᴜᴘᴘᴏʀᴛ ᴀɴᴅ ᴜᴘᴅᴀᴛᴇꜱ🎑
+
+<a href="https://t.me/tmmainchannel"><img src="https://img.shields.io/badge/Join-Main%20Channel-green.svg?style=for-the-badge&logo=Telegram">
+<a href="https://t.me/technomindzchat"><img src="https://img.shields.io/badge/Join-Support%20Group-blue.svg?style=for-the-badge&logo=Telegram">
+
+## MODAL ROBOT
+ * [<i><b>@TMTaggerBot</i></b>](https://t.me/tmtaggerbot)
+
